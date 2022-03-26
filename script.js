e65465bb432b4c423db4f376b0bd5f34b5721c41
mn46 <m.nowak033@gmail.com>
2022-03-26 13:21:40 +0100

    colors done!

--- script.js
+++ script.js
@@ -20,8 +20,23 @@ function showBikes(bikes) {
     clone.querySelector(".brand").textContent = bikes.brand;
     clone.querySelector(".model").textContent = bikes.title.rendered;
     clone.querySelector(".price").textContent = bikes.price;
-    clone.querySelector(".colors").textContent = bikes.color;
+
+    // color
+    clone.querySelector(".color1").style.backgroundColor = bikes.color1;
+
+    if (bikes.color1 === "") {
+        clone.querySelector(".color1").textContent = "N/A";
+    }
+
+    clone.querySelector(".color2").style.backgroundColor = bikes.color2;
+    clone.querySelector(".color3").style.backgroundColor = bikes.color3;
+    clone.querySelector(".color4").style.backgroundColor = bikes.color4;
+    clone.querySelector(".color5").style.backgroundColor = bikes.color5;
+    
+    // clone.querySelector(".colors").textContent = bikes.color;
     clone.querySelector(".availability").textContent = bikes.availability;
 
     document.querySelector("#bike_list").appendChild(clone);
+
 }
+
